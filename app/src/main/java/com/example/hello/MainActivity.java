3dac61d51ac938dc0a5442f361daeaf3cd3a1425
Anim Yusufbhai Malvat <malvat@uwindsor.ca>
2020-03-09 21:30:57 -0400

    add int variable

--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int d = 0;
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
